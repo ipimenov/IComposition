8f1774ee537d3319204a1a7fee5df8aaed92ca3f
Maverick <bravo1@list.ru>
2023-06-27 12:47:33 +0300

    Создан Presentation слой, ресурсы, ViewBinding, навигация через FragmentManager, Parcelable

--- app/src/main/java/ru/ipimenov/training/icomposition/domain/entity/GameResult.kt
+++ app/src/main/java/ru/ipimenov/training/icomposition/domain/entity/GameResult.kt
@@ -1,8 +1,12 @@
 package ru.ipimenov.training.icomposition.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 class GameResult(
     val winner: Boolean,
     val countOfRightAnswers: Int,
     val countOfQuestions: Int,
     val gameSettings: GameSettings
-)
+) : Parcelable
---
+++ app/src/main/java/ru/ipimenov/training/icomposition/presentation/WelcomeFragment.kt
@@ -0,0 +1,44 @@
+package ru.ipimenov.training.icomposition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.ipimenov.training.icomposition.R
+import ru.ipimenov.training.icomposition.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == nul")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btUnderstand.setOnClickListener {
+            launchChooseLevelFragment()
+        }
+    }
+
+    private fun launchChooseLevelFragment() {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, ChooseLevelFragment.newInstance())
+            .addToBackStack(ChooseLevelFragment.NAME)
+            .commit()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
