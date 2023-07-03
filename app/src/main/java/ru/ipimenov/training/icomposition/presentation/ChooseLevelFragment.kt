6de541c4db123ae431bfff9317aeca1fd7417221
Maverick <bravo1@list.ru>
2023-07-03 23:21:28 +0300

    Создана навигация с помощью jetpack navigation, получен рабочий вариант приложения 3

--- app/src/main/java/ru/ipimenov/training/icomposition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/ru/ipimenov/training/icomposition/presentation/ChooseLevelFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.ipimenov.training.icomposition.R
+import androidx.navigation.fragment.findNavController
 import ru.ipimenov.training.icomposition.databinding.FragmentChooseLevelBinding
 import ru.ipimenov.training.icomposition.domain.entity.Level
 import java.lang.RuntimeException
@@ -44,23 +44,13 @@ class ChooseLevelFragment : Fragment() {
     }
 
     private fun launchGameFragment(level: Level) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFragment.newInstance(level))
-            .addToBackStack(GameFragment.NAME)
-            .commit()
+        findNavController().navigate(
+            ChooseLevelFragmentDirections.actionChooseLevelFragmentToGameFragment(level)
+        )
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    companion object {
-
-        const val NAME = "ChooseLevelFragment"
-
-        fun newInstance(): ChooseLevelFragment {
-            return ChooseLevelFragment()
-        }
     }
 }
